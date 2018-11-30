8754484dcc485593b1635034b6a6fb09f8e70621
VitorTakara <vtakaraa@gmail.com>
2018-11-30 17:29:23 -0200

    Inserido dashboard_relatorio

---
+++ dashboard_relatorio/app/js/sweetAlertConfig.js
@@ -0,0 +1,30 @@
+﻿// type: 'deletar'
+// url: url de onde vai fazer o delete
+
+function mySweetAlert() {
+    swal({
+        title: "Você tem certeza?",
+        text: "Você tem certeza de que deseja realizar esta ação?",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        reverseButtons:true,
+        confirmButtonText: 'Confirmar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+       if(result.value)
+            swal(
+               'Sucesso!',
+               'Você realizou esta ação! :)',
+               'success'
+            )
+         else{
+            swal(
+               'Cancelado!',
+               'Operação cancelada',
+               'error'
+            )
+         }
+    })
+}
---
+++ dashboard_relatorio/app/js/toasterConfig.js
@@ -0,0 +1,23 @@
+﻿// type: 'danger', 'success'
+// message: mensagem a ser exibida
+// url: url a ser re-direcionada
+
+function showToaster(message, type = "danger", url = '') {
+    if(message){
+
+        if(type == '')
+            type = 'danger'; 
+
+        $.toaster({
+            priority: type,
+            message: message,
+            settings: {
+                timeout: 5000
+            }
+        });
+
+        // Se não ter URL, não vai ter redirecionamento
+        if (url != '')
+            window.location.href = url;
+    }     
+}
